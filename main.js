a2571400d4ed98362d0c6ad606164dae937fee87
BenScott <benscott92@gmail.com>
2020-06-26 14:12:07 -0400

    basic javascript added

---
+++ main.js
@@ -0,0 +1,23 @@
+let coin = {
+    state: 0,
+    flip: function() {
+        /* 1. Randomly set your coin object's "state" property to be either 
+           0 or 1: use "this.state" to access the "state" property on this object.*/
+           if(Math.random() > .5){
+               this.state = 1
+           } else{
+               this.state = 0
+               console.log('gabumon')
+           }
+    },
+    toString: function() {
+        /* 2. Return the string "Heads" or "Tails", depending on whether
+           "this.state" is 0 or 1. */
+    },
+    toHTML: function() {
+        let image = document.createElement('img');
+        /* 3. Set the properties of this image element to show either face-up
+           or face-down, depending on whether this.state is 0 or 1.*/
+        return image;
+    }
+};
